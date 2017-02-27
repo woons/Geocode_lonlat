6eff33c673afe551ed9e46fb0cebab0c65aba929
Yeowoon <woons.2016@gmail.com>
2017-02-27 22:43:42 +0900

    Update data.frame

--- code.R
+++ code.R
@@ -21,16 +21,27 @@ getData <- function(url){
   return (latlon)
 }
 
+# sample data.frame
+my_address <- data.frame(address = c("제주 특별자치도 제주시 첨단로 242", "부산광역시 동래구 안락1동 921-5", "서울특별시 구로구 공원로6가길"),
+                         category = c("회사", "자택", "자택"),
+                         etc = c("woons", NA, NA)
+)
+
+
+
 # sample vector
-my_address <- c("제주 특별자치도 제주시 첨단로 242", "부산광역시 동래구 안락1동 921-5", "서울특별시 구로구 공원로6가길")
+sample_vector <- c("제주 특별자치도 제주시 첨단로 242", "부산광역시 동래구 안락1동 921-5", "서울특별시 구로구 공원로6가길")
 
 # empty vector
 final_lonlat <- NULL
 
 # loop
-for(i in my_address){
+for(i in my_address$address){
   xml_address <- getUrls(i, "xml")
   local_lonlat <- getData(xml_address)
   final_lonlat <- rbind(final_lonlat, local_lonlat)
 }
 
+#final
+final <- cbind(my_address, final_lonlat)
+final
